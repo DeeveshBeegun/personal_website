9204d6094f9acdc0f0714586492ab291e5ec8ef2
Deevesh Beegun <35701065+DeeveshBeegun@users.noreply.github.com>
2026-03-21 21:08:12 +0400

    Add current project and current book

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -5,6 +5,16 @@ function About() {
             <p>
                 Hi 👋, I am a Java developer with more than 4 years of experience in developing, configuring, and building Java applications. I am adept in technologies like Kubernetes and Kafka.
             </p>
+
+            <h2>Current Project</h2>
+            <p>
+                I am currently working on a cypto-ai-agent and a kafka tool for visualisation.
+            </p>
+
+            <h2>Current Book</h2>
+            <p>
+                I am currently Quiet: The Power of Introverts in a World That Can't Stop Talking
+            </p>
         </section>
     );
 }
