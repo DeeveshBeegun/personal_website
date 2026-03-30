3acf84d5de360f2ff46f9ef280aa58cfa4ee8c4b
Deevesh Beegun <35701065+DeeveshBeegun@users.noreply.github.com>
2026-03-30 21:03:44 +0400

    Add google analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import Header from "./components/Header";
 import About from "./components/About";
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
+
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('G-L8PVE881L9');
+  })
   return (
     <div style={{ padding: "20px", fontFamily: "Arial" }}>
       <Header />
