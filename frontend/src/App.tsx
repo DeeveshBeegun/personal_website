c5ac4f1ea3f7ca7adb3c394d79867a2675156c73
Deevesh Beegun <35701065+DeeveshBeegun@users.noreply.github.com>
2026-03-30 21:11:01 +0400

    Remove reactGA

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,7 @@
 import Header from "./components/Header";
 import About from "./components/About";
-import { useEffect } from 'react';
-import ReactGA from 'react-ga4';
-
 
 function App() {
-  useEffect(() => {
-    ReactGA.initialize('G-L8PVE881L9');
-  })
   return (
     <div style={{ padding: "20px", fontFamily: "Arial" }}>
       <Header />
